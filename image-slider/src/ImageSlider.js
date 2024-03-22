808bff914e5405911ca0ee2cd0b99836c9d832cc
Swastik <swastikpanda32@gmail.com>
2024-03-22 11:49:48 +0530

    added timeout functionality to load the img after 5 secs

--- image-slider/src/ImageSlider.js
+++ image-slider/src/ImageSlider.js
@@ -1,12 +1,5 @@
-import React, { useState } from "react";
-
-const data = [
-  "https://static.vecteezy.com/system/resources/previews/032/242/170/non_2x/beautiful-waterfall-flowers-water-nature-waterfall-hd-wallpaper-ai-generated-free-photo.jpg",
-  "https://cdn.pixabay.com/photo/2018/08/14/13/23/ocean-3605547_640.jpg",
-  "https://t3.ftcdn.net/jpg/05/62/54/02/360_F_562540214_unXDmpcHlTdImYhQE7dPBXZEWfFyAeeH.jpg",
-  "https://images.alphacoders.com/134/1342799.png",
-  "https://e1.pxfuel.com/desktop-wallpaper/556/915/desktop-wallpaper-how-to-install-reactjs-frontend.jpg",
-];
+import React, { useEffect, useState } from "react";
+import { data } from "./constants";
 
 const ImageSlider = () => {
   const [activeIndex, setActiveIndex] = useState(0);
@@ -18,6 +11,16 @@ const ImageSlider = () => {
   const handleNextClick = () => {
     setActiveIndex((activeIndex + 1) % data.length);
   };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      handleNextClick();
+    }, 5000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [activeIndex]);
 
   return (
     <div
@@ -39,6 +42,7 @@ const ImageSlider = () => {
         </button>
         {data.map((url, i) => (
           <img
+            key={url}
             src={url}
             alt="wallpaper images"
             style={{
---
+++ image-slider/src/constants.js
@@ -0,0 +1,7 @@
+export const data = [
+  "https://static.vecteezy.com/system/resources/previews/032/242/170/non_2x/beautiful-waterfall-flowers-water-nature-waterfall-hd-wallpaper-ai-generated-free-photo.jpg",
+  "https://cdn.pixabay.com/photo/2018/08/14/13/23/ocean-3605547_640.jpg",
+  "https://t3.ftcdn.net/jpg/05/62/54/02/360_F_562540214_unXDmpcHlTdImYhQE7dPBXZEWfFyAeeH.jpg",
+  "https://images.alphacoders.com/134/1342799.png",
+  "https://e1.pxfuel.com/desktop-wallpaper/556/915/desktop-wallpaper-how-to-install-reactjs-frontend.jpg",
+];
